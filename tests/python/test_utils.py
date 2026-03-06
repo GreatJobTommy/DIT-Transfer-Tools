42e8f28387d09e7f5990dadf5d0de43363fcd4b4
Tom <accounts@storymotion.de>
2026-03-06 12:20:29 +0100

    chore: v4.8.0 release

--- tests/python/test_utils.py
+++ tests/python/test_utils.py
@@ -1,8 +1,10 @@
+from .utils import get_dir_size
+
 def test_get_dir_size(tmp_path):
-    d = tmp_path / &quot;d&quot;
+    d = tmp_path / "d"
     d.mkdir()
-    (d / &quot;f1&quot;).write_bytes(b&quot;a&quot;)
-    sub = d / &quot;sub&quot;
+    (d / "f1").write_bytes(b"a")
+    sub = d / "sub"
     sub.mkdir()
-    (sub / &quot;f2&quot;).write_bytes(b&quot;bb&quot;)
+    (sub / "f2").write_bytes(b"bb")
     assert get_dir_size(d) == 1 + 2
---
+++ tests/python/utils.py
@@ -0,0 +1,8 @@
+import os
+
+def get_dir_size(path):
+    total = 0
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            total += os.path.getsize(os.path.join(root, file))
+    return total
